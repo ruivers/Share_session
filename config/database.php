650ccae59a87b7dd3c127f9a1f09d75781f0aaf1
aruis <ruiweilai@163.com>
2018-01-03 15:01:12 +0800

    添加 redis 驱动

--- config/database.php
+++ config/database.php
@@ -112,6 +112,12 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
+            'database' => 1,
+        ],
+        'session' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
             'database' => 0,
         ],
 
